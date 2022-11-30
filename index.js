fde6bf35850e966ce046fdd4b06aec8e37147ee0
yu1k <48705423+yu1k@users.noreply.github.com>
2022-11-30 23:17:20 +0900

    change: IPアドレスを取得する機能を./index.jsから./lib/ip.jsに移動。モジュールとして作成しました。

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const axios = require('axios');
 const cron = require('node-cron');
 
+const ip = require("./lib/ip");
+
 /**
  * 必要なクレデンシャルをチェック
  * ない場合は process.exit(1) する。
@@ -17,22 +19,6 @@ if (!(process.env.DDNS_USERNAME || process.env.DDNS_PASSWORD || process.env.DDNS
 }
 
 const GOOGLE_DOMAINS_ENDPOINT_URL = 'https://domains.google.com/nic/update';
-const IPINFO_IO_URL = 'https://ipinfo.io/';
-
-/**
- * 現在のIPアドレスを取得する
- * request for https://ipinfo.io/
- **/
-async function getGlobalIpAddr() {
-    try {
-        let host_info = await axios.get(IPINFO_IO_URL);
-        console.log('host_ip_addr: ' + host_info.data.ip);
-        return host_info;
-    } catch (error) {
-        console.error('status: ' + error);
-        return;
-    }
-}
 
 // username:password をBase64エンコードする
 const basicAuthInfo = Buffer.from(`${process.env.DDNS_USERNAME}:${process.env.DDNS_PASSWORD}`).toString('base64');
@@ -47,8 +33,8 @@ async function postDdnsUpdateAPI() {
     }
 
     //IPドレスを取得する
-    let host_info = await getGlobalIpAddr();
-    let host_ip_addr = host_info.data.ip;
+    let host_info = await ip.getGlobalIpAddress();
+    let host_ip_addr = host_info;
     // デバッグ用
     // console.log('host_ip_addr' + host_ip_addr);
 
