4289c79118a8502860243994870a015f38f64c68
yu1k <48705423+yu1k@users.noreply.github.com>
2022-12-03 07:52:49 +0900

    update: index.js内での変数をできるだけキャメルケースに揃えた, 変数名を更新した, コメントを追加した, デバッグ用のconsole.logを消した, 条件に一致した場合のみSlackへ通知する処理に変更しました。そのほか細かい更新をしました。

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const ENV_FILE_PATH = path.join(__dirname, "./config.env");
 require("dotenv").config({ path: ENV_FILE_PATH });
 
 const ip = require("./lib/ip");
+const dns = require("./lib/dns");
 
 /**
  * 必要なクレデンシャルをチェック
@@ -28,8 +29,12 @@ const GOOGLE_DOMAINS_ENDPOINT_URL = 'https://domains.google.com/nic/update';
 const basicAuthInfo = Buffer.from(`${process.env.DDNS_USERNAME}:${process.env.DDNS_PASSWORD}`).toString('base64');
 console.log(basicAuthInfo);
 
-// DDNS のDNSレコードを更新する
-async function postDdnsUpdateAPI() {
+/**
+ * DNSレコードを更新する関数
+ * IPv4アドレスの場合は Aレコードが更新される
+ * IPv6アドレスの場合は AAAA レコードが更新される
+*/
+async function updateDnsRecord() {
     const GOOGLE_DDNS_OPTION_HEADERS = {
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': `Basic ${basicAuthInfo}`,
@@ -37,22 +42,27 @@ async function postDdnsUpdateAPI() {
     }
 
     //IPドレスを取得する
-    let host_info = await ip.getGlobalIpAddress();
-    let host_ip_addr = host_info;
-    // デバッグ用
-    // console.log('host_ip_addr' + host_ip_addr);
+    let hostIpAddress = await ip.getGlobalIpAddress();
 
-    // ログ用の時間を格納する
+    // DNSレコードを更新する前に更新前のIPアドレスを格納する
+    let ipAddressBeforeUpdate = null;
+    dns.getIpAddressViaDnsLookup(process.env.DDNS_HOSTNAME, ip.getIpAddressType(process.env.DDNS_HOSTNAME)).then((data) => {
+        data = ipAddressBeforeUpdate;
+    });
+
+    // ログ用の時間を格納する変数
     let currentTime = null;
 
     try {
         // ログ用
         currentTime = new Date();
 
-        const response = await axios.post(`${GOOGLE_DOMAINS_ENDPOINT_URL}?hostname=${process.env.DDNS_HOSTNAME}&myip=${host_ip_addr}`, {}, { 'headers': GOOGLE_DDNS_OPTION_HEADERS });
-        // ref: https://support.google.com/domains/answer/6147083?hl=ja
+        // https://support.google.com/domains/answer/6147083?hl=ja
+        const response = await axios.post(`${GOOGLE_DOMAINS_ENDPOINT_URL}?hostname=${process.env.DDNS_HOSTNAME}&myip=${hostIpAddress}`, {}, { 'headers': GOOGLE_DDNS_OPTION_HEADERS });
+        // ログを表示させる
         console.log('response Status: ' + response.data + "\n" + 'current time: ' + currentTime);
 
+        // 文字列のtrue, falseをBooleanに変換して SLACK_NOTICE_FLAG変数 に格納する
         // 最初のリクエスト以外で response.data が good だった場合に該当の response.data をSlackに投げる
         // ...
         let SLACK_WEBHOOK_OPTIONS_HEADERS = {
@@ -61,9 +71,13 @@ async function postDdnsUpdateAPI() {
             }
         };
 
+
         if ((response.data).match(/good/)) {
-            const slack_post = await axios.post(process.env.SLACK_WEBHOOK_URL, { 'channel': '#bot-test', 'username': 'DDNS更新確認くん', 'text': `${response.data}`, 'icon_emoji': ':memo:' }, { 'headers': SLACK_WEBHOOK_OPTIONS_HEADERS });
-            console.log('slack response Status: ' + slack_post.data);
+            // 現在のIPアドレスがDNSレコード更新前のIPアドレスと異なっていた場合にSlackへ通知する
+            if (hostIpAddress != ipAddressBeforeUpdate) {
+                const slackPost = await axios.post(process.env.SLACK_WEBHOOK_URL, { 'channel': '#bot-test', 'username': 'DDNS更新確認くん', 'text': `${response.data}\nIPアドレスは更新されました。`, 'icon_emoji': ':memo:' }, { 'headers': SLACK_WEBHOOK_OPTIONS_HEADERS });
+                console.log('slack response Status: ' + slackPost.data);
+            }
         }
 
     } catch (error) {
@@ -72,8 +86,8 @@ async function postDdnsUpdateAPI() {
 }
 
 // 初回の実行はcronを用いず行う
-postDdnsUpdateAPI();
+updateDnsRecord();
 console.log('initialize, current time: ' + new Date());
 
 // 10分ごとに実行する
-setInterval(postDdnsUpdateAPI, 600000);
+setInterval(updateDnsRecord, 600000);
